ea9cdaa94499191fed0b5e54b592c8ea89e3c837
masumrashid <razzshikder@gmail.com>
2025-03-09 17:39:19 +0600

    New

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/app.dart';
 
-void main(){
-runApp(const TaskManagerApp());
+void main() {
+  runApp(const TaskManagerApp());
 }
