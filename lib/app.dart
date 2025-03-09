fbaaa052563681038a40f4bb6b493dc4210afbc9
masumrashid <razzshikder@gmail.com>
2025-03-09 17:39:42 +0600

    New

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/ui/screens/splash_screen.dart';
 
-
 class TaskManagerApp extends StatelessWidget {
   const TaskManagerApp({super.key});
 
